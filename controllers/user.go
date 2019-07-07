ce5def9a334609209a5f8c48ff3cd5d43bce877f
DingZhenyuan <ding_zy@outlook.com>
2019-07-07 19:39:27 +0800

    加入路由和主页

--- controllers/user.go
+++ controllers/user.go
@@ -1,1 +1,44 @@
 package controllers
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/szdx4/attsys-server/config"
+	"github.com/szdx4/attsys-server/requests"
+	"github.com/szdx4/attsys-server/response"
+	"time"
+)
+
+// UserAuth 用户认证
+func UserAuth(c *gin.Context) {
+	var req requests.UserAuthRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.BadRequest(c, "Bad Request")
+		c.Abort()
+		return
+	}
+
+	// 验证提交数据的合法性
+	user, err := req.Validate()
+	if err != nil {
+		response.Unauthorized(c, err.Error())
+		c.Abort()
+		return
+	}
+
+	// 生成新的 JWT Token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id": user.ID,
+		//"role":       user.Role,
+		"expired_at": time.Now().Add(time.Hour * time.Duration(config.App.TokenValid)).Format(time.RFC3339),
+	})
+	tokenString, err := token.SignedString([]byte(config.App.EncryptKey))
+	if err != nil {
+		response.InternalServerError(c, "Token sign error")
+		c.Abort()
+		return
+	}
+
+	// 发送响应
+	response.UserAuth(c, user.ID, tokenString)
+}
--- routers/router.go
+++ routers/router.go
@@ -1,1 +1,33 @@
 package routers
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/szdx4/attsys-server/config"
+	"github.com/szdx4/attsys-server/controllers"
+)
+
+// Router 设置路由和公共中间件，返回 Gin Engine 对象
+func Router() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"*"}
+	corsConfig.AllowHeaders = []string{"*"}
+	corsConfig.AllowMethods = []string{"*"}
+	r.Use(cors.New(corsConfig))
+
+	gin.SetMode(config.App.RunMode)
+
+	r.GET("/", controllers.Home)
+
+	// User
+	// 用户认证
+	r.POST("/user/auth", controllers.UserAuth)
+
+	//r.POST("/user", controllers.)
+
+	return r
+}
